de37e1c82371fec7b17b7572e4ffe656dcde88b1
Brendan LaCroix <brendan@animoto.com>
2015-10-27 13:32:38 -0700

    Add intern testing framework.
    Closes #1.

---
+++ tests/units/polish-process-stylesheet-tests.js
@@ -0,0 +1,87 @@
+define([
+    'require',
+    'intern/chai!',
+    'intern/dojo/node!sinon-chai',
+    'intern/dojo/node!sinon',
+    'intern/dojo/node!../../lib/polish-process-stylesheet'
+  ],
+  function (require, chai, sinonChai, sinon, processStylesheet) {
+    var registerSuite     = require('intern!object'),
+        assert            = require('intern/chai!assert'),
+        fs                = require('intern/dojo/node!fs'),
+        polish            = require('intern/dojo/node!../../lib/polish');
+
+    chai.use(sinonChai);
+
+    registerSuite(function() {
+      return {
+        name: 'polish-process-stylesheet',
+
+        beforeEach: function() {
+
+        },
+        afterEach: function() {
+
+        },
+
+        'test that attempting to polish a non-CSS, SCSS, Sass, or LESS file throws an error': function() {
+          var deferred = this.async(3000),
+              output;
+
+          fs.readFile('./tests/test_helpers/not-a-stylesheet.js', deferred.callback(function(error, notStylesheet) {
+            if (error) {
+              throw error;
+            }
+
+            try {
+              output = processStylesheet(notStylesheet.toString('utf8'), './tests/test_helpers/not-a-stylesheet.js');
+            } catch(e) {
+              assert.strictEqual(e.message, 'Polish only accepts css, scss, sass, and less files.');
+            }
+          }));
+        },
+        'test that the output has the file name and the AST': function() {
+          var deferred = this.async(3000),
+              output;
+
+          fs.readFile('./tests/test_helpers/scss.scss', deferred.callback(function(error, stylesheet) {
+            if (error) {
+              throw error;
+            }
+
+            output = processStylesheet(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss');
+
+            assert.property(output, 'ast');
+            assert.isObject(output.ast);
+            assert.property(output, 'name');
+            assert.isString(output.name);
+          }));
+        },
+        'test that inline config comments are appended to adjacent rules': function() {
+          var deferred = this.async(3000),
+              idCount = 0,
+              output,
+              errors;
+
+          fs.readFile('./tests/test_helpers/css.css', deferred.callback(function(error, stylesheet) {
+            if (error) {
+              throw error;
+            }
+
+            output = processStylesheet(stylesheet.toString('utf8'), './tests/test_helpers/css.css');
+            errors = polish(stylesheet.toString('utf8'), './tests/test_helpers/css.css', { plugins: ['polish-plugin-no-styling-ids','polish-plugin-no-styling-elements'] });
+
+            output.ast.traverse(function(node) {
+              if (node.type === 'id') {
+                idCount++;
+              }
+            });
+
+            assert.strictEqual(idCount, 3);
+            assert.strictEqual(errors.length, 2);
+          }));
+        }
+      };
+    });
+  }
+);
---
+++ tests/units/polish-tests.js
@@ -0,0 +1,82 @@
+define(function (require) {
+  var registerSuite = require('intern!object'),
+      assert        = require('intern/chai!assert'),
+      fs            = require('intern/dojo/node!fs'),
+      polish        = require('intern/dojo/node!../../lib/polish');
+
+  registerSuite({
+    name: 'polish',
+    'test that PolishCSS errors without plugins specified': function() {
+      var deferred = this.async(3000);
+
+      fs.readFile('./tests/test_helpers/scss.scss', deferred.callback(function(error, stylesheet) {
+        if (error) {
+          throw error;
+        }
+
+        try {
+          polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', {});
+        } catch(e) {
+          assert.strictEqual(e.message, 'Polish requires a list of plugins. Please specify a list of modules or the path to a directory containing a set of plugins.');
+        }
+      }));
+    },
+    'test that PolishCSS errors on empty stylesheets': function() {
+      var deferred = this.async(3000);
+
+      fs.readFile('./tests/test_helpers/empty.css', deferred.callback(function(error, stylesheet) {
+        if (error) {
+          throw error;
+        }
+
+        try {
+          polish(stylesheet.toString('utf8'), './tests/test_helpers/empty.css', { plugins: ['polish-plugin-something'] });
+        } catch(e) {
+          assert.strictEqual(e.message, 'Polish cannot lint empty files: ./tests/test_helpers/empty.css');
+        }
+      }));
+    },
+    'test that PolishCSS will run with a plugin': function() {
+      var deferred = this.async(3000),
+          errors;
+
+      fs.readFile('./tests/test_helpers/scss.scss', deferred.callback(function(error, stylesheet) {
+        if (error) {
+          throw error;
+        }
+
+        errors = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', { plugins: ['polish-plugin-no-styling-ids'] });
+
+        assert.isArray(errors);
+        assert.strictEqual(errors.length, 1);
+        assert.isObject(errors[0]);
+        assert.property(errors[0], 'file');
+        assert.property(errors[0], 'rule');
+        assert.property(errors[0].rule, 'name');
+        assert.property(errors[0].rule, 'message');
+        assert.property(errors[0], 'data');
+        assert.property(errors[0].data, 'rule');
+      }));
+    },
+    'test that PolishCSS allows ignoring plugins': function() {
+      var deferred = this.async(3000),
+          errors;
+
+      fs.readFile('./tests/test_helpers/scss.scss', deferred.callback(function(error, stylesheet) {
+        if (error) {
+          throw error;
+        }
+
+        errors = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', { plugins: ['polish-plugin-no-styling-ids','polish-plugin-no-styling-elements'], ignoredPlugins: ['no-styling-elements'] });
+
+        assert.isArray(errors);
+        assert.strictEqual(errors.length, 1);
+        assert.strictEqual(errors[0].rule.name, 'no-styling-ids');
+      }));
+    },
+    'test that Polish exports helper methods': function() {
+      assert.property(polish, 'reporter');
+      assert.property(polish, 'getPlugins');
+    }
+  });
+});
