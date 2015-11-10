927f403c7729ed8e0aead7efc924a4c0d2817553
Brendan LaCroix <brendan@animoto.com>
2015-11-10 01:24:01 -0500

    tests: Cover warnings in reporter as well as errors, test for excerpts of AST nodes that are too long to reasonably print

--- tests/units/polish-error-reporter-tests.js
+++ tests/units/polish-error-reporter-tests.js
@@ -37,7 +37,7 @@ define(['require', 'intern/chai!', 'intern/dojo/node!sinon-chai', 'intern/dojo/n
           chai.expect(consoleLogStub).to.not.be.called;
         }));
       },
-      'test that it reports an error': function() {
+      'test that it reports errors and warnings': function() {
         var deferred = this.async(3000),
             results;
 
@@ -46,19 +46,23 @@ define(['require', 'intern/chai!', 'intern/dojo/node!sinon-chai', 'intern/dojo/n
             throw error;
           }
 
-          results = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', [ { module: 'polish-no-styling-ids', severity: 2 } ]);
+          results = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', [ { module: 'polish-no-styling-ids', severity: 2 }, { module: 'polish-no-styling-elements', severity: 1 }, { module: 'polish-match-stylesheet-names-to-rules', severity: 0 } ]);
 
           polish.reporter('./tests/test_helpers/scss.scss', results.errors, results.warnings);
 
           chai.expect(consoleLogStub).to.be.calledOnce;
           /* should report the filename */
           chai.expect(consoleLogStub).and.calledWithMatch(results.filePath);
-          /* should report the line number */
+          /* should report the line numbers */
           chai.expect(consoleLogStub).to.be.calledWithMatch(results.errors[0].error.node.start.line + ':' + results.errors[0].error.node.start.column);
+          chai.expect(consoleLogStub).to.be.calledWithMatch(results.warnings[0].error.node.start.line + ':' + results.warnings[0].error.node.start.column);
           /* should report the failed node as a string */
           chai.expect(consoleLogStub).to.be.calledWithMatch('#no-styling-ids');
+          chai.expect(consoleLogStub).to.be.calledWithMatch('div');
           /* should report the error message */
           chai.expect(consoleLogStub).to.be.calledWithMatch(results.errors[0].plugin.message);
+          chai.expect(consoleLogStub).to.be.calledWithMatch(results.warnings[0].plugin.message);
+          chai.expect(consoleLogStub).to.not.be.calledWithMatch('Do not use multiple top-level rules in a file.');
         }));
       },
       'test that it reports multiple errors grouped under the filename': function() {
@@ -97,7 +101,7 @@ define(['require', 'intern/chai!', 'intern/dojo/node!sinon-chai', 'intern/dojo/n
             throw error;
           }
 
-          results = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', [ { module: 'polish-no-styling-ids', severity: 2 }, { module: 'polish-no-styling-elements', severity: 2 }, { module: 'polish-match-stylesheet-names-to-rules', severity: 2 } ]);
+          results = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', [ { module: 'polish-no-styling-ids', severity: 2 }, { module: 'polish-no-styling-elements', severity: 2 }, { module: 'polish-match-stylesheet-names-to-rules', severity: 1 } ]);
 
           polish.reporter('./tests/test_helpers/scss.scss', results.errors, results.warnings);
 
@@ -114,12 +118,38 @@ define(['require', 'intern/chai!', 'intern/dojo/node!sinon-chai', 'intern/dojo/n
           /* should report the error message */
           chai.expect(consoleLogStub).to.be.calledWithMatch(results.errors[0].plugin.message);
           chai.expect(consoleLogStub).to.be.calledWithMatch(results.errors[1].plugin.message);
-          chai.expect(consoleLogStub).to.be.calledWithMatch(results.errors[2].plugin.message({
+          chai.expect(consoleLogStub).to.be.calledWithMatch(results.warnings[0].plugin.message({
             error : {
               errorName: 'failed_on_multiple',
               message: ''
             }
           }));
+        }));
+      },
+      'test that excerpts that are too long are condensed': function() {
+          var deferred = this.async(3000),
+              results;
+
+        fs.readFile('./tests/test_helpers/scss.scss', deferred.callback(function(error, stylesheet) {
+          if (error) {
+            throw error;
+          }
+
+          results = polish(stylesheet.toString('utf8'), './tests/test_helpers/scss.scss', [ { module: 'polish-no-styling-ids', severity: 2 }, { module: 'polish-no-styling-elements', severity: 2 }, { module: 'polish-match-stylesheet-names-to-rules', severity: 2 } ]);
+
+          results.errors[0].error.node = {
+            start : {
+              line   : 1,
+              column : 1
+            },
+            toString : function() {
+              return '123456789012345678901234567890123456789012345678901234567890';
+            }
+          };
+
+          polish.reporter('./tests/test_helpers/scss.scss', results.errors, results.warnings);
+
+          chai.expect(consoleLogStub).to.be.calledWithMatch('123456789012345678901234567890...');
         }));
       }
     };
